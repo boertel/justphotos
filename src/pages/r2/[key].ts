b341dd964041f1697ec633775175fb049067d5b4
boertel <ben@comediadesign.com>
2024-08-23 14:31:41 -0700

    probably have to be cache at the function level

--- src/pages/r2/[key].ts
+++ src/pages/r2/[key].ts
@@ -4,5 +4,9 @@ export async function GET({ request, params, locals }: APIContext) {
   const { R2 } = locals.runtime.env;
   const object = await R2.get(params.key);
   const data = await object.arrayBuffer();
-  return new Response(data);
+  return new Response(data, {
+    headers: {
+      "Cache-Control": "public, max-age=3600, s-maxage=3600, immutable",
+    },
+  });
 }
