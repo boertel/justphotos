1929526430bc675dcb6527c2ac7ce178c20687f2
boertel <ben@comediadesign.com>
2024-08-24 10:52:00 -0700

    try ts-ignore

--- src/pages/r2/[key].ts
+++ src/pages/r2/[key].ts
@@ -11,19 +11,22 @@ export async function GET({ params: { key }, locals }: APIContext) {
 
   let cacheResponse = await cache.match(key);
   if (cacheResponse) {
+    console.log(`Cache hit for ${key}`);
     return cacheResponse;
   }
+  console.log(`Cache miss for ${key}`);
   const object = await R2.get(key);
   if (object === null) {
     return new Response("Not Found", { status: 404 });
   }
   const headers = new Headers();
   object.writeHttpMetadata(headers);
-  headers.set("Cache-Control", "s-maxage=3600, immutable");
+  headers.set("Cache-Control", "s-maxage=10, immutable");
 
   const data = await object.arrayBuffer();
   const response = new Response(data, { headers });
 
+  //@ts-ignore
   ctx.waitUntil(cache.put(key, response.clone()));
 
   return response;
