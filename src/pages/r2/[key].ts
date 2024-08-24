3b38c431766f9731cd325fbbe8225001b1e9fd7f
boertel <ben@comediadesign.com>
2024-08-24 10:48:20 -0700

    rely on cache for r2/key

--- src/pages/r2/[key].ts
+++ src/pages/r2/[key].ts
@@ -1,12 +1,30 @@
 import type { APIContext } from "astro";
 
-export async function GET({ request, params, locals }: APIContext) {
-  const { R2 } = locals.runtime.env;
-  const object = await R2.get(params.key);
+export async function GET({ params: { key }, locals }: APIContext) {
+  const {
+    env: { R2 },
+    caches,
+    ctx,
+  } = locals.runtime;
+
+  const cache = caches.default;
+
+  let cacheResponse = await cache.match(key);
+  if (cacheResponse) {
+    return cacheResponse;
+  }
+  const object = await R2.get(key);
+  if (object === null) {
+    return new Response("Not Found", { status: 404 });
+  }
+  const headers = new Headers();
+  object.writeHttpMetadata(headers);
+  headers.set("Cache-Control", "s-maxage=3600, immutable");
+
   const data = await object.arrayBuffer();
-  return new Response(data, {
-    headers: {
-      "Cache-Control": "public, max-age=3600, s-maxage=3600, immutable",
-    },
-  });
+  const response = new Response(data, { headers });
+
+  ctx.waitUntil(cache.put(key, response.clone()));
+
+  return response;
 }
