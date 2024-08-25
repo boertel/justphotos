47612732b84d8c7a81745b39d9f825be44e7fd23
boertel <ben@comediadesign.com>
2024-08-25 08:16:25 -0700

    cache all R2 calls

--- src/pages/r2/[key].ts
+++ src/pages/r2/[key].ts
@@ -1,38 +1,17 @@
 import type { APIContext } from "astro";
 
+import { cache, CachedResponse } from "../../utils";
+
 export async function GET({ request, params: { key }, locals }: APIContext) {
-  const {
-    env: { R2 },
-    caches,
-    ctx,
-  } = locals.runtime;
-
-  const cache = caches.default;
-
-  const url = new URL(request.url);
-  const cacheKey = new Request(url.toString(), request);
-
-  //@ts-ignore
-  let cacheResponse = await cache.match(cacheKey);
-  if (cacheResponse) {
-    console.log(`Cache hit for ${key}`);
-    return cacheResponse;
-  }
-  console.log(`Cache miss for ${key}`);
-  const object = await R2.get(key);
-  if (object === null) {
-    return new Response("Not Found", { status: 404 });
-  }
-  const headers = new Headers();
-  object.writeHttpMetadata(headers);
-  headers.set("etag", object.httpEtag);
-  headers.append("Cache-Control", "s-maxage=10, immutable");
-
-  const data = await object.arrayBuffer();
-  const response = new Response(data, { headers });
-
-  //@ts-ignore
-  ctx.waitUntil(cache.put(cacheKey, response.clone()));
-
-  return response;
+  return await cache({ request, locals }, async ({ R2 }) => {
+    const object = await R2.get(key);
+    if (!object) {
+      throw new Error(`Object not found: ${key}`);
+    }
+    return new CachedResponse(await object.arrayBuffer(), {
+      headers: {
+        etag: object.httpEtag,
+      },
+    });
+  });
 }
---
+++ src/utils.ts
@@ -0,0 +1,44 @@
+export async function cache({ request, locals }, callback) {
+  const {
+    env: { R2 },
+    caches,
+    ctx,
+  } = locals.runtime;
+
+  const _cache = caches.default;
+
+  const url = new URL(request.url);
+  const cacheKey = new Request(url.toString(), request);
+
+  //@ts-ignore
+  let cacheResponse = await _cache.match(cacheKey);
+  if (cacheResponse) {
+    console.log(`Cache hit for ${cacheKey.url.toString()}`);
+    return cacheResponse;
+  }
+  console.log(`Cache miss for ${cacheKey.url.toString()}`);
+
+  try {
+    const response = await callback({ R2 });
+
+    //@ts-ignore
+    ctx.waitUntil(_cache.put(cacheKey, response.clone()));
+    return response;
+  } catch (exception) {
+    console.error(exception);
+    return new Response(exception.message, { status: 404 });
+  }
+}
+
+export class CachedResponse extends Response {
+  constructor(body, options = { headers: {} }) {
+    const formattedBody =
+      body instanceof ArrayBuffer ? body : JSON.stringify(body);
+    super(formattedBody, {
+      headers: {
+        ...options.headers,
+        "Cache-Control": "s-maxage=10, immutable",
+      },
+    });
+  }
+}
