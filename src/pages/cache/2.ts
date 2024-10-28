05723d4659ae81a89317a451587b199ea82d67a9
boertel <ben@comediadesign.com>
2024-10-28 16:48:11 -0700

    more cache

--- src/pages/cache/2.ts
+++ src/pages/cache/2.ts
@@ -1,7 +1,7 @@
 import type { APIContext } from "astro";
 
 export async function GET({ request }: APIContext) {
-  return new Response(`${new Date().toLocaleString()}<a href="/2">2</a>`, {
+  return new Response(`${new Date().toLocaleString()}<a href="/1">1</a>`, {
     headers: {
       "Content-Type": "text/html",
       "Cache-Control": `public, max-age=10, s-maxage=20`,
